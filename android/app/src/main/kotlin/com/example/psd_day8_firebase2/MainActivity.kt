7023ad4f6bb8dc97866fc8fcb7e10827aa2e2384
khaledhosni <meckawy@gmail.com>
2023-10-04 13:22:17 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/psd_day8_firebase2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.psd_day8_firebase2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/demo_app.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    printToken();
+
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+  printToken()async{
+    final fcmToken = await FirebaseMessaging.instance.getToken();
+
+    print(fcmToken);
+  }
+}
